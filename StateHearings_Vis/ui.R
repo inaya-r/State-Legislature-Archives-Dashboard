1eae2e9fce3ff6e6880892fc03df6cac56e69cff
inaya-r <rizin22@outlook.com>
2024-11-14 21:51:42 -0800

    formatting ui file

--- StateHearings_Vis/ui.R
+++ StateHearings_Vis/ui.R
@@ -1,9 +1,10 @@
 library("shiny")
 library("plotly")
 library("shinydashboard")
+library("readxl")
 
 # Define UI for application that draws a histogram
-dashboardPage(
+ui <- dashboardPage(
     skin = "black", 
     
     # App title 
@@ -23,5 +24,42 @@ dashboardPage(
         selectInput("state", "Select State", choices = sort(state.name))
       )
     ),
+    
+    # body 
+    dashboardBody(
+      tabItems(
+        
+        # about tab content
+        tabItem(tabName = "about",
+                fluidRow(
+                  box(title = "About This App", width = 12, status = "info",
+                      solidHeader = TRUE,
+                      "This dashboard provides insights into state legislatures, 
+                        including committees per state and other trends.")
+                )
+        ),
+        
+        #dashboard tab content 
+        tabItem(tabName = "dashboard",
+          fluidRow(
+            # heat map
+            box(title = "US Heatmap: Number of Committees by State", width = 12, 
+                status = "primary", plotlyOutput("us_map", height = "600px")
+            )
+          ),
+          
+          fluidRow(
+            # other visual 
+            box(title = "other vis 1", width = 6, status = "warning", 
+                "vis 1 content"
+            ),
+            box(title = "other vis 2", width = 6, status = "warning",
+                "vis 2 content"
+            )
+        )
+  
+      )
+    )
       
+  )
 )
